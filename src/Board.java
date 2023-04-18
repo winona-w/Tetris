6684ce16b628bb8ef2fb7efe3d2e604bb51e7d4d
winona-w <wherleyws21@gcc.edu>
2023-04-18 14:42:15 -0400

    added some class variables to Board

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,8 @@
 public class Board {
     final int NUM_ROWS = -1;
     final int NUM_COLS = -1;
+    int numLinesCleared = 0;
+    int totalPoints = 0;
 
     public Board() {
 
@@ -28,5 +30,9 @@ public class Board {
 
     public boolean topRowIsntEmpty() {
         return false;
+    }
+
+    public void updateScore() {
+
     }
 }
